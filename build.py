9cc7932d172e02239b0a54e3085a5b0c9dca8ba7
Liang Hai <lianghai@gmail.com>
2014-10-06 13:19:48 +0530

    Initial commit
    To be published to GitHub.

---
+++ build.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+from subprocess import call
+from os.path    import exists
+
+
+family_name = 'XDevanagari'
+
+style_name_list = [
+  'Light',
+  'Regular',
+  'Medium',
+  'SemiBold',
+  'Bold',
+]
+
+
+def main():
+
+  call(['rm', '-fr', 'build'])
+  call(['mkdir', 'build'])
+
+  for style_name in style_name_list:
+
+    otf_path = 'build/%s-%s.otf' % (family_name, style_name)
+    style_dir = 'styles/' + style_name
+
+    call(['makeotf'] + makeotf_arg_list)
+
+    call(['rm', '-f', style_dir + '/current.fpr'])
+
+    if exists(otf_path):
+      call(['cp', '-f', otf_path, '/Library/Application Support/Adobe/Fonts'])
+
+
+makeotf_arg_list = [
+  '-f', style_dir + '/font.ufo',
+  '-o', otf_path,
+  '-mf', 'FontMenuNameDB',
+  '-gf', 'GlyphOrderAndAliasDB',
+  '-r',
+  '-shw',
+]
+
+
+if __name__ == '__main__':
+  main()
